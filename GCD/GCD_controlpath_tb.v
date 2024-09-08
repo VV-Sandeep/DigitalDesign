2a4090e6fdf6d4d240dde3f025c2d501e4bdc1c3
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-08 23:51:02 +0530

    Update GCD_controlpath_tb.v

--- GCD/GCD_controlpath_tb.v
+++ GCD/GCD_controlpath_tb.v
@@ -1,1 +1,17 @@
-
+module GCD_controlpath_tb();
+reg clk,rst,go,gt,lt,eq;
+wire asel,bsel,aload,bload,out_en;
+GCD_controlpath dut(clk,rst,go,gt,lt,eq,asel,bsel,aload,bload,out_en);
+initial clk = 0;
+always #5 clk = ~clk;
+initial
+begin
+    rst = 1'b1; gt = 1;
+    #15 rst = 0;
+    #12 go = 1'b1;
+    #20 lt = 0; gt = 1;
+    #13 gt = 0;lt = 1;
+    #24 eq = 1;lt = 0; go = 0;
+    #50 $finish;
+end
+endmodule
