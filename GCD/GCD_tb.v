5f4384517e1deb867aa2d62cc72532754bce09ff
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-08 23:50:18 +0530

    Update GCD_tb.v

--- GCD/GCD_tb.v
+++ GCD/GCD_tb.v
@@ -1,1 +1,14 @@
-
+module GCD_TB();
+reg clk,rst,go;
+reg [7:0]a,b;
+wire [7:0]out;
+GCD dut(clk,rst,go,a,b,out);
+initial clk = 0;
+always #5 clk = ~clk;
+initial
+begin
+rst = 1; a = 8'd96; b = 8'd72;go = 1;
+#18 rst = 0;
+#200 $finish;
+end
+endmodule
