e34465bd6d6a1ba8c20f5c9cd332ad44040f6ce9
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-08 23:52:09 +0530

    Update register8bit.v

--- GCD/register8bit.v
+++ GCD/register8bit.v
@@ -1,1 +1,12 @@
-
+module register8bit(clk,rst,load,in,out);
+input clk,rst,load;
+input [7:0]in;
+output reg [7:0]out;
+always @(posedge clk)
+begin
+if (rst)
+out <= 0;
+else if (load)
+out <= in;
+end
+endmodule
