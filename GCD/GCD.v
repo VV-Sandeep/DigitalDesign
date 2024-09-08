045a5f20cf774b09786b61147729610528fb2b52
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-08 23:49:35 +0530

    Update GCD.v

--- GCD/GCD.v
+++ GCD/GCD.v
@@ -1,1 +1,8 @@
-
+module GCD(clk,rst,go,a,b,out);
+input clk,rst,go;
+input [7:0]a,b;
+output [7:0]out;
+wire asel,bsel,aload,bload,out_en,gt,lt,eq;
+GCD_datapaths A1(a,b,clk,rst,asel,bsel,aload,bload,out_en,gt,lt,eq,out);
+GCD_controlpath A2(clk,rst,go,gt,lt,eq,asel,bsel,aload,bload,out_en);
+endmodule
